a8d7b0d897444416516b53e057d05e06b06b695b
Joel Klint <joel.klint@gmail.com>
2017-08-15 09:04:47 +0200

    E guild has better color in guild picker

--- priv/react/src/components/GuildPickerButton/GuildPickerButton.js
+++ priv/react/src/components/GuildPickerButton/GuildPickerButton.js
@@ -2,25 +2,26 @@ import React, { Component } from 'react'
 import './GuildPickerButton.css'
 
 class GuildPickerButton extends Component {
-    render() {
+  render() {
 
-        const style = {
-            backgroundColor: this.props.guild.color,
-        }
-        if(this.props.guild.name === 'E') {
-            style.color = 'white'
-        }
-
-        return (
-            <div 
-                className="GuildPickerButton_Base"
-                style={this.props.active ? style : {}}
-                onClick={this.props.onClick}
-            >
-                {this.props.guild.name}
-            </div>
-        )
+    const style = {
+      backgroundColor: this.props.guild.color,
     }
+    if(this.props.guild.name === 'E') {
+      style.color = 'white'
+      style.backgroundColor = 'black'
+    }
+
+    return (
+      <div
+        className="GuildPickerButton_Base"
+        style={this.props.active ? style : {}}
+        onClick={this.props.onClick}
+      >
+        {this.props.guild.name}
+      </div>
+    )
+  }
 }
 
 export default GuildPickerButton
