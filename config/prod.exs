682c3bbfc446ebaeebcf1c010d53e5b2103a6576
Joel Klint <joel.klint@gmail.com>
2017-08-15 19:48:53 +0200

    Changed where phoenix looks for asset manifest

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ config :nollning_score, NollningScore.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "young-woodland-64561.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json",
+  # This does not seem to work
+  cache_static_manifest: "priv/react/build/asset-manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 
