dee60f82ffddf201252de8296121406417b74ac8
Joel Klint <joel.klint@gmail.com>
2017-08-17 22:18:56 +0200

    Added min and max limits for interval in frontend

--- priv/react/src/components/IntegerInput/IntegerInput.js
+++ priv/react/src/components/IntegerInput/IntegerInput.js
@@ -1,11 +1,48 @@
 import React from 'react'
 import './IntegerInput.css'
+import R from 'ramda'
 
 class IntegerInput extends React.Component {
 
-  increment = () => this.props.sendScoreToBackend((this.props.score.value || 0) + 1)
+  increment = () => {
+    if(!this.maxLimitReached()) {
+      this.props.sendScoreToBackend((this.props.score.value || 0) + 1)
+    }
+  }
 
-  decrement = () => this.props.sendScoreToBackend((this.props.score.value || 0) - 1)
+  decrement = () => {
+    if(!this.minLimitReached()) {
+      this.props.sendScoreToBackend((this.props.score.value || 0) - 1)
+    }
+  }
+
+  maxLimitReached = () => {
+    const { category, score } = this.props
+    if(R.isNil(category.interval_max)) {
+      return false
+    }
+    else if(score.value < category.interval_max) {
+      return false
+    }
+    else {
+      console.error("Reached max level. This limit is not implemented in backend")
+      return true
+    }
+  }
+
+  minLimitReached = () => {
+    const { category, score } = this.props
+    if(R.isNil(category.interval_min)) {
+      return false
+    }
+    else if(score.value > category.interval_min) {
+      return false
+    }
+    else {
+      console.error("Reached min level. This limit is not implemented in backend")
+      return true
+    }
+  }
 
   render() {
     const { score } = this.props
@@ -13,7 +50,7 @@ class IntegerInput extends React.Component {
     return (
       <div className="IntegerInput_Base">
         <div
-          className="Button"
+          className={`Button ${this.minLimitReached() ? 'Disabled_Button' : ''}`}
           onTouchTap={() => this.decrement()}
         >
           -
@@ -22,7 +59,7 @@ class IntegerInput extends React.Component {
         <div className="Score">{typeof(score.value) === 'number' ? score.value : "-"}</div>
 
         <div
-          className="Button"
+          className={`Button ${this.maxLimitReached() ? 'Disabled_Button' : ''}`}
           onTouchTap={() => this.increment()}
         >
           +
