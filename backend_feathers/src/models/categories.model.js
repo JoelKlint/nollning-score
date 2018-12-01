eb80095a5df40698b91171be76a833396763f9ff
Joel Klint <joel.klint@gmail.com>
2018-12-01 19:03:50 +0100

    created initial categories service

---
+++ backend_feathers/src/models/categories.model.js
@@ -0,0 +1,53 @@
+// See http://docs.sequelizejs.com/en/latest/docs/models-definition/
+// for more of what you can do here.
+const Sequelize = require('sequelize');
+const DataTypes = Sequelize.DataTypes;
+
+module.exports = function (app) {
+  const sequelizeClient = app.get('sequelizeClient');
+  const categories = sequelizeClient.define('categories', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    type: {
+      type: DataTypes.ENUM(
+        'interval',
+        'integer',
+        'boolean',
+        'guild'
+      )
+    },
+    absolute: {
+      type: DataTypes.BOOLEAN
+    },
+    global: {
+      type: DataTypes.BOOLEAN
+    },
+    weight: {
+      type: DataTypes.INTEGER
+    },
+    interval_min: {
+      type: DataTypes.INTEGER
+    },
+    interval_max: {
+      type: DataTypes.INTEGER
+    }
+
+  }, {
+    hooks: {
+      beforeCount(options) {
+        options.raw = true;
+      }
+    }
+  });
+
+  // eslint-disable-next-line no-unused-vars
+  categories.associate = function (models) {
+    // Define associations here
+    // See http://docs.sequelizejs.com/en/latest/docs/associations/
+  };
+
+  return categories;
+};
--- backend_feathers/src/services/index.js
+++ backend_feathers/src/services/index.js
@@ -1,9 +1,11 @@
 const users = require('./users/users.service.js');
 const guilds = require('./guilds/guilds.service.js');
 const events = require('./events/events.service.js');
+const categories = require('./categories/categories.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(guilds);
   app.configure(events);
+  app.configure(categories);
 };
