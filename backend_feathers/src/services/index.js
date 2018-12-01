157f4db64103fb2dc9848911261dbebe1d80583f
Joel Klint <joel.klint@gmail.com>
2018-12-01 19:06:32 +0100

    created initial scores service

--- backend_feathers/src/services/index.js
+++ backend_feathers/src/services/index.js
@@ -2,10 +2,12 @@ const users = require('./users/users.service.js');
 const guilds = require('./guilds/guilds.service.js');
 const events = require('./events/events.service.js');
 const categories = require('./categories/categories.service.js');
+const scores = require('./scores/scores.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(guilds);
   app.configure(events);
   app.configure(categories);
+  app.configure(scores);
 };
