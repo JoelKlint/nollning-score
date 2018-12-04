134527b9659cb71d560a7a237ebb642022f1a219
Joel Klint <joel.klint@gmail.com>
2018-12-04 18:59:34 +0100

    fixed db seeder

--- backend_feathers/seeds.js
+++ backend_feathers/seeds.js
@@ -1,10 +1,27 @@
 const app = require('./src/app');
 
-app.service('users').create({
-  email: 'test@test.test',
-  password: 'test'
-}).catch(err => {
-  console.error(err);
+const seed = () => new Promise((resolve, reject) => {
+  Promise.resolve()
+    .then(() => app.service('users').create({
+      email: 'test@test.test',
+      password: 'test'
+    }))
+    .then(() => app.service('events').create([
+      { name: 'FlyING' },
+      { name: 'Lådbilsrally' },
+      { name: 'Överphöshinderbana' },
+      { name: 'Regatta' },
+    ]))
+    .then(resolve).catch(reject);
 });
 
-process.exit(0);
+seed()
+  .then(() => process.exit(0))
+  .catch(() => process.exit(-1));
+
+
+
+
+
+
+
